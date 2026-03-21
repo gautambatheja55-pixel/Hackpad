65f2b04fa992a64ecb607a3638815f189390d889
Gautam Batheja <gautambatheja55@gmail.com>
2026-03-21 10:55:10 +0530

    Update Hackpad.py

--- Code/Hackpad.py
+++ Code/Hackpad.py
@@ -1,34 +1,29 @@
 import usb_hid
 import rotaryio
 import digitalio
-import pwmio
 import board
+import neopixel
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.mouse import Mouse
 import time
 
-# RGB LED
-led_r = pwmio.PWMOut(board.A0)
-led_g = pwmio.PWMOut(board.A1)
-led_b = pwmio.PWMOut(board.A2)
-
-def set_color(r, g, b):
-    led_r.duty_cycle = int(r / 255 * 65535)
-    led_g.duty_cycle = int(g / 255 * 65535)
-    led_b.duty_cycle = int(b / 255 * 65535)
-
-set_color(255, 0, 0)
+led = neopixel.NeoPixel(board.A0, 1, brightness=0.2, pixel_order=neopixel.GRBW)
+led[0] = (255, 0, 0, 0)
 
 keyboard = Keyboard(usb_hid.devices)
 keyboard.release_all()
 mouse = Mouse(usb_hid.devices)
 
-key1 = digitalio.DigitalInOut(board.D10)  # Ctrl
-key2 = digitalio.DigitalInOut(board.D9)   # C
-key3 = digitalio.DigitalInOut(board.D8)   # V
+RED = (255, 0, 0, 0)
+GREEN = (0, 255, 0, 0)
+ORANGE = (255, 165, 0, 0)
 
-for key in [key1, key2, key3]:
+ctrl_key = digitalio.DigitalInOut(board.D10)
+c_key = digitalio.DigitalInOut(board.D9)
+v_key = digitalio.DigitalInOut(board.D8)
+
+for key in [ctrl_key, c_key, v_key]:
     key.direction = digitalio.Direction.INPUT
     key.pull = digitalio.Pull.UP
 
@@ -36,61 +31,55 @@ encoder = rotaryio.IncrementalEncoder(board.A3, board.A4)
 encoder.position = 0
 last_position = 0
 
-encoder_btn = digitalio.DigitalInOut(board.A5)
-encoder_btn.direction = digitalio.Direction.INPUT
-encoder_btn.pull = digitalio.Pull.UP
+encoder_button = digitalio.DigitalInOut(board.A5)
+encoder_button.direction = digitalio.Direction.INPUT
+encoder_button.pull = digitalio.Pull.UP
 
-# Track previous states
-prev_key1 = True
-prev_key2 = True
-prev_key3 = True
-prev_btn = True
+prev_ctrl = True
+prev_c = True
+prev_v = True
+prev_encoder_button = True
 
 while True:
-    # Key 1 - Ctrl (non-blocking)
-    if not key1.value and prev_key1:
+    if not ctrl_key.value and prev_ctrl:
         keyboard.press(Keycode.CONTROL)
-        set_color(0, 255, 0)
-    elif key1.value and not prev_key1:
+        led[0] = GREEN
+    elif ctrl_key.value and not prev_ctrl:
         keyboard.release(Keycode.CONTROL)
-        set_color(255, 0, 0)
-    prev_key1 = key1.value
+        led[0] = RED
+    prev_ctrl = ctrl_key.value
 
-    # Key 2 - C
-    if not key2.value and prev_key2:
+    if not c_key.value and prev_c:
         keyboard.press(Keycode.C)
-        set_color(0, 255, 0)
-    elif key2.value and not prev_key2:
+        led[0] = GREEN
+    elif c_key.value and not prev_c:
         keyboard.release(Keycode.C)
-        set_color(255, 0, 0)
-    prev_key2 = key2.value
+        led[0] = RED
+    prev_c = c_key.value
 
-    # Key 3 - V
-    if not key3.value and prev_key3:
+    if not v_key.value and prev_v:
         keyboard.press(Keycode.V)
-        set_color(0, 255, 0)
-    elif key3.value and not prev_key3:
+        led[0] = GREEN
+    elif v_key.value and not prev_v:
         keyboard.release(Keycode.V)
-        set_color(255, 0, 0)
-    prev_key3 = key3.value
+        led[0] = RED
+    prev_v = v_key.value
 
-    # Rotary encoder scroll
     current_position = encoder.position
     if current_position != last_position:
-        set_color(255, 165, 0)
+        led[0] = ORANGE
         diff = (current_position - last_position) // 2
         if diff != 0:
             mouse.move(wheel=-diff)
             last_position = current_position
         time.sleep(0.05)
-        set_color(255, 0, 0)
+        led[0] = RED
 
-    # Encoder button - left click
-    if not encoder_btn.value and prev_btn:
-        set_color(255, 165, 0)
+    if not encoder_button.value and prev_encoder_button:
+        led[0] = ORANGE
         mouse.click(Mouse.LEFT_BUTTON)
-    elif encoder_btn.value and not prev_btn:
-        set_color(255, 0, 0)
-    prev_btn = encoder_btn.value
+    elif encoder_button.value and not prev_encoder_button:
+        led[0] = RED
+    prev_encoder_button = encoder_button.value
 
     time.sleep(0.01)
